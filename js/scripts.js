66e79b149d3b383155f3b32303949fd0c21f2c77
Isaac Moreno <isaacmoreno@Isaacs-Mac-mini.local>
2021-04-02 15:21:07 -0700

    remove some js code

--- js/scripts.js
+++ js/scripts.js
@@ -2,57 +2,16 @@ Business Logic
 
 const fullString = ["0", "Beep!", "Boop!", "Wont you be my neighbor?", "4", "5", "6", "7", "8", "9"]
 
-
 fullString.forEach(elements => {
-  if elements = 
-
   console.log(elements)
 })
-
-for (let i = 1; i <=9; i++) {
-  if i % 1 === 0 
-}
-
-
-
-
-
-
-function fullString(numberInput)
-  let array = [];
-  if (fullString.includes("0")) {
-    return 0; {
-  } else if {
-      return 
-    }
-  }
-
-  return array;
-
-
-
-const fullString = {
-  '0': '0',
-  '1': 'Beep!',
-  '2': 'Boop!',
-  '3': 'Wont you be my neighbor', 
-  '4': '4', 
-  '5': '5', 
-  '6': '6', 
-  '7': '7', 
-  '8': '8', 
-  '9': '9', 
-};
-
-fullString[];
-
 
 
 // UI Logic
 // $(document).ready(function() {
   
 //   $("#number-form").submit(function(event) {
-//     const numberInput = $("input#number").val();
+//     const numberInput = $("#number").val();
 //     $(".number-result").text(numberInput);
 //     $("#output").show();
 //     event.preventDefault();
